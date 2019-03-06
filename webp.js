3651a29cbcd7decdf45dfdddef0d59979c01d23d
Wilbert Caba <wilbertcaba@gmail.com>
2019-03-06 11:30:16 -0500

    First commit, I guess

--- webp.js
+++ webp.js
@@ -1,8 +1,8 @@
 var imagemin = require("imagemin"),    // The imagemin module.
   webp = require("imagemin-webp"),   // imagemin's WebP plugin.
-  outputFolder = "./img",            // Output folder
-  PNGImages = "./img/*.png",         // PNG images
-  JPEGImages = "./img/*.jpg";        // JPEG images
+  outputFolder = "dist/assets/media/images",            // Output folder
+  PNGImages = "dist/assets/media/images/*.png",         // PNG images
+  JPEGImages = "dist/assets/media/images/*.jpg";        // JPEG images
 
 imagemin([PNGImages], outputFolder, {
   plugins: [webp({
@@ -12,6 +12,6 @@ imagemin([PNGImages], outputFolder, {
 
 imagemin([JPEGImages], outputFolder, {
   plugins: [webp({
-    quality: 65 // Quality setting from 0 to 100
+    quality: 85 // Quality setting from 0 to 100
   })]
 });
