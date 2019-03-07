1f3f37ad5d16b37083d9c607b1531470418e45b5
Wilbert Caba <wilbertcaba@gmail.com>
2019-03-07 15:36:06 -0500

    Very basic structure of the whole page implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bs = require('browser-sync').create();
 bs.init({
     server: "dist",
     files: ["dist/assets/styles/*.css"],
+    notify:false,
     plugins: [
         {
             module: "bs-html-injector",
